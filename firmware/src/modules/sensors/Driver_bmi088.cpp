eb5bed6bbe995774c4368856fb25170898a8e576
Filip Gawlik <filippo.gawlik@gmail.com>
2026-05-07 15:03:14 +0200

    Changed gyro rate

--- firmware/src/modules/sensors/Driver_bmi088.cpp
+++ firmware/src/modules/sensors/Driver_bmi088.cpp
@@ -13,7 +13,7 @@ void Driver_bmi088::initialize()
     bmi088_acc_set_range(&m_AccDevice, BMI088_ACC_RANGE_6G);
 
     bmi088_gyro_init_spi(&m_GyroDevice, CFG_SPI, CFG_PIN_CS_BMI_GYRO);
-    bmi088_gyro_set_bandwidth(&m_GyroDevice, BMI088_GYRO_ODR_2000_BW_523HZ);
+    bmi088_gyro_set_bandwidth(&m_GyroDevice, BMI088_GYRO_ODR_1000_BW_116HZ);
     bmi088_gyro_set_range(&m_GyroDevice, BMI088_GYRO_RANGE_500DPS);
 }
 
